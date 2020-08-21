22d12968bf296c6e33b4edc06a3a3636e669c369
Jiahong Long <jiahong888long@gmail.com>
2020-08-20 18:15:00 -0700

    clipboard context capability

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod types;
 use log::{info, warn};
+use clipboard::*;
 use argparse::{
     ArgumentParser,
     StoreTrue,
@@ -16,27 +17,22 @@ fn main() {
         .map(|()| log::set_max_level(log::LevelFilter::Info));
 
     let mut verbose = false;
+    let mut clipboard = false;
     let mut file = String::new();
     let mut sponge_v: Vec<String> = Vec::new();
-    let mut failed = false;
-    let mut err: i32 = 0;
 
     { // limit argparse scope
         let mut ap = ArgumentParser::new();
         ap.set_description("Generates sPoNgEbOB text.");
         ap.refer(&mut verbose)
             .add_option(&["-v", "--verbose"], StoreTrue, "Log output");
+        ap.refer(&mut clipboard)
+            .add_option(&["-c", "--clipboard"], StoreTrue, "Saves output to system clipboard");
         ap.refer(&mut file)
             .add_option(&["-f", "--file"], Store, "Sponge an entire file");
         ap.refer(&mut sponge_v)
             .add_argument("string", Collect, "String to be sponged (ignored if file provided");
-        match ap.parse_args() {
-            Err(x) => {
-                failed = true;
-                err = x;
-            }
-            _ => { }
-        }
+        ap.parse_args_or_exit();
     }
 
     // collect posargs
@@ -48,17 +44,12 @@ fn main() {
     let sponge = String::from(sponge.trim());
 
     // check if failed
-    if failed {
-        warn!("Argument parser failed with");
-        warn!("verbose: {}", verbose);
-        warn!("file:    {}", file);
-        warn!("sponge:  {}", sponge);
-        std::process::exit(err);
-    } else if sponge.len() == 0 {
+    if sponge.len() == 0 {
         warn!("Please provide the text to be sponged.");
         std::process::exit(3);
     }
 
+    // log
     if verbose {
         info!("verbose: {}", verbose);
         info!("file:    {}", file);
@@ -75,7 +66,6 @@ fn main() {
     let mut sm = types::StateMachine::new();
     let mut sponged: String = String::new();
     for c in content.chars() {
-        eprintln!("{}", c);
         // allow symbols and numbers
         if !c.is_alphabetic() {
             sponged.push(c);
@@ -88,6 +78,17 @@ fn main() {
                     break;
                 }
                 if types::EXCEPTIONS[s] == c {
+                    if c.is_uppercase() {
+                        sponged.push_str(&c
+                            .to_lowercase()
+                            .to_string());
+                        sm.consec_down += 1;
+                    } else {
+                        sponged.push_str(&c
+                            .to_uppercase()
+                            .to_string());
+                        sm.consec_up += 1;
+                    }
                     was_except = true;
                     break;
                 }
@@ -95,18 +96,32 @@ fn main() {
             if !was_except {
                 if sm.next_is_upper() {
                     sm.consec_up += 1;
-                    sponged.push(c
+                    sponged.push_str(&c
                         .to_uppercase()
-                        .collect::<Vec<_>>()[0]);
+                        .to_string());
                 } else {
                     sm.consec_down += 1;
-                    sponged.push(c
+                    sponged.push_str(&c
                         .to_lowercase()
-                        .collect::<Vec<_>>()[0]);
+                        .to_string());
                 }
             }
         }
     }
 
-    eprintln!("{}", sponged);
+    println!("{}", sponged);
+
+    if clipboard {
+        let ctx: Result<ClipboardContext, Box<dyn std::error::Error>> =
+            ClipboardProvider::new();
+        match ctx.ok() {
+            Some(mut ctx) => {
+                ctx.set_contents(sponged.to_owned()).unwrap();
+                if verbose {
+                    info!("{:?}", ctx.get_contents());
+                }
+            }
+            _ => {}
+        }
+    }
 }
